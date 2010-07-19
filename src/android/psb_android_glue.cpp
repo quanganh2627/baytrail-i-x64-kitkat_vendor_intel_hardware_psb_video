6cf6c23cad231d482e22b86e0d7afa523a67dbb0
mgross <mgross@mtgdev.jf.intel.com>
2010-07-19 10:17:46 -0700

    revert this hack when enable ISurface for texture streaming
    Change-Id: I54a7c5b85c2ee7ea13fe61d7c8b11d5081794858

--- src/android/psb_android_glue.cpp
+++ src/android/psb_android_glue.cpp
@@ -2,16 +2,16 @@
 #include <binder/ProcessState.h>
 #include <binder/IServiceManager.h>
 #include <utils/Log.h>
-#include <ui/Surface.h>
-#include <ui/ISurface.h>
-#include <ui/SurfaceComposerClient.h>
+#include <surfaceflinger/Surface.h>
+#include <surfaceflinger/ISurface.h>
+#include <surfaceflinger/SurfaceComposerClient.h>
 #include <binder/MemoryHeapBase.h>
 #include "psb_android_glue.h"
 
 using namespace android;
 
 sp<ISurface> isurface;
-ISurface::BufferHeap mBufferHeap;
+//ISurface::BufferHeap mBufferHeap;
 
 unsigned char* psb_android_registerBuffers(void** android_isurface, int pid, int width, int height)
 {
@@ -25,13 +25,14 @@ unsigned char* psb_android_registerBuffers(void** android_isurface, int pid, int
 	return 0;
     }
 
-    mBufferHeap = ISurface::BufferHeap(width, height, width, height, PIXEL_FORMAT_RGB_565, heap);
+    //mBufferHeap = ISurface::BufferHeap(width, height, width, height, PIXEL_FORMAT_RGB_565, heap);
 
     isurface = static_cast<ISurface*>(*android_isurface);
 
-    isurface->registerBuffers(mBufferHeap);
+//    isurface->registerBuffers(mBufferHeap);
 
-    return static_cast<uint8_t*>(mBufferHeap.heap->base());
+//    return static_cast<uint8_t*>(mBufferHeap.heap->base());
+    return (unsigned char *) 0xff;
 }
 
 void psb_android_postBuffer(int offset)
@@ -45,20 +46,20 @@ void psb_android_clearHeap()
     if (isurface.get())
     {
 	isurface->unregisterBuffers();
-	mBufferHeap.heap.clear();	
+//	mBufferHeap.heap.clear();	
     }
 }
 
 void psb_android_register_isurface(void** android_isurface, int srcw, int srch)
 {
     isurface = static_cast<ISurface*>(*android_isurface);
-    isurface->createTextureStreamSource();
-    isurface->setTextureStreamID(0);
-    isurface->setTextureStreamDim(srcw, srch);
+//bugbug mgross was here:     isurface->createTextureStreamSource();
+//bugbug mgross was here:     isurface->setTextureStreamID(0);
+//bugbug mgross was here:     isurface->setTextureStreamDim(srcw, srch);
 }
 
 void psb_android_texture_streaming_display(int buffer_index)
 {
-    isurface->displayTextureStreamBuffer(buffer_index);
+//bugbug mgross was here: refactored base classs we need to redo this    isurface->displayTextureStreamBuffer(buffer_index);
 }
 
