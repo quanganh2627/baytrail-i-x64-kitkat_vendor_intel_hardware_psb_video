c5a64077cb39291bcb932cdd501fa3b9e0d735cf
Ji Guoliang <guoliang.ji@gmail.com>
2012-06-29 19:39:33 +0800

    assign the stride with graphic->stride instead of width
    BZ: 44326
    
    On the widi mode the surface was create by widi in the use space, the
    stride was assigned as the video width.  However after the video enhancement
    implemented, the camera source transmit the data which has padding to
    the encoder, make the width different with the stride.
    So modify the stride assignment to fix the display problem.
    Signed-off-by: Ji Guoliang <guoliang.ji@gmail.com>
    
    Change-Id: Ie70c89fc56f180b289266de58bd9c0bb53d34037
    Reviewed-on: http://android.intel.com:8080/54829
    Reviewed-by: Ding, Haitao <haitao.ding@intel.com>
    Tested-by: Ding, Haitao <haitao.ding@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/psb_surface_attrib.c
+++ src/psb_surface_attrib.c
@@ -57,17 +57,15 @@ VAStatus psb_surface_create_from_ub(
             return VA_STATUS_ERROR_ALLOCATION_FAILED;
         }
 
+        psb_surface->stride = graphic_buffers->luma_stride;
         if (0) {
             ;
         } else if (512 == graphic_buffers->luma_stride) {
             psb_surface->stride_mode = STRIDE_512;
-            psb_surface->stride = 512;
         } else if (1024 == graphic_buffers->luma_stride) {
             psb_surface->stride_mode = STRIDE_1024;
-            psb_surface->stride = 1024;
         } else if (1280 == graphic_buffers->luma_stride) {
             psb_surface->stride_mode = STRIDE_1280;
-            psb_surface->stride = 1280;
 #ifdef PSBVIDEO_MSVDX_DEC_TILING
             if (graphic_buffers->tiling) {
                 psb_surface->stride_mode = STRIDE_2048;
@@ -76,13 +74,10 @@ VAStatus psb_surface_create_from_ub(
 #endif
         } else if (2048 == graphic_buffers->luma_stride) {
             psb_surface->stride_mode = STRIDE_2048;
-            psb_surface->stride = 2048;
         } else if (4096 == graphic_buffers->luma_stride) {
             psb_surface->stride_mode = STRIDE_4096;
-            psb_surface->stride = 4096;
         } else {
             psb_surface->stride_mode = STRIDE_NA;
-            psb_surface->stride = width;
         }
         if (psb_surface->stride != graphic_buffers->luma_stride) {
             return VA_STATUS_ERROR_ALLOCATION_FAILED;
