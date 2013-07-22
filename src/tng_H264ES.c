21d0411383c2726696ed6aa4f6864c6844ec081c
Ren Zhaohan <zhaohan.ren@intel.com>
2013-07-22 12:57:41 +0800

    Enable disable bitstuffing
    BZ: 124869
    
    This patch enabled disable bitstuffing according to bit_stuffing_dis
    option in misc RC parameters.
    
    Change-Id: I34eb15a6023d1248aae0690aa05109b3c22db2e8
    Signed-off-by: Ren Zhaohan <zhaohan.ren@intel.com>
    Reviewed-on: http://android.intel.com:8080/119985
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/tng_H264ES.c
+++ src/tng_H264ES.c
@@ -248,6 +248,13 @@ static VAStatus tng__H264ES_process_misc_ratecontrol_param(context_ENC_p ctx, ob
         "%s: rate control changed from %d to %d\n", __FUNCTION__,
         psRCParams->ui32BitsPerSecond, psMiscRcParams->bits_per_second);
 
+    if (psMiscRcParams->rc_flags.value != 0) {
+       if (psMiscRcParams->rc_flags.bits.disable_bit_stuffing)
+           ctx->sRCParams.bDisableBitStuffing = IMG_TRUE;
+       drv_debug_msg(VIDEO_DEBUG_GENERAL, "bDisableBitStuffing is %d\n",
+           ctx->sRCParams.bDisableBitStuffing);
+    }
+
     if (psMiscRcParams->window_size > 2000) {
 	drv_debug_msg(VIDEO_DEBUG_ERROR, "window_size is too much!\n");
 	return VA_STATUS_ERROR_INVALID_PARAMETER;
