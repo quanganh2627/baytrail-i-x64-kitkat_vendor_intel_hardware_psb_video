58dbdcfa07afa3f9db78792e92ddd121c730852d
Li Zeng <li.zeng@intel.com>
2013-11-13 08:27:18 +0800

    Video: remove width check for tiling/linear selection
    BZ: 151378
    
    remove width check for tiling/linear selection, use libva's tiling flag instead
    
    Change-Id: Id0243556ef0a16ad7ac7978a92e8e97a7f5f31c0
    Signed-off-by: Li Zeng <li.zeng@intel.com>

--- src/android/psb_surface_gralloc.c
+++ src/android/psb_surface_gralloc.c
@@ -352,9 +352,8 @@ VAStatus psb_CreateSurfacesFromGralloc(
             break;
         }
 
-#ifdef PSBVIDEO_MSVDX_DEC_TILING
-        if (width > 1280)
-            external_buffers->tiling = 1;
+#ifndef PSBVIDEO_MSVDX_DEC_TILING
+        external_buffers->tiling = 0;
 #endif
         /*hard code the gralloc buffer usage*/
         usage = GRALLOC_USAGE_HW_TEXTURE | GRALLOC_USAGE_HW_COMPOSER;
