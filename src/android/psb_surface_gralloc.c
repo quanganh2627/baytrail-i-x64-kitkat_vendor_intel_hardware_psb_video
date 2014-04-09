90206bdc6332fc30b3bcfced2285e2df20dca042
Tianmi Chen <tianmi.chen@intel.com>
2014-04-09 15:29:46 +0800

    psb_video: assign initial values to crop width and crop height
    BZ: 183181
    
    HEVC is sw codec, which would not call setRenderRect() in MediaSDK,
    so crop width and crop heigh are not set. The solution is to assign
    an initial value to crop width and height.
    
    Change-Id: Ibdcd985938e4a7eea7cd67b8573ee9b4d17c6874
    Signed-off-by: Tianmi Chen <tianmi.chen@intel.com>

--- src/android/psb_surface_gralloc.c
+++ src/android/psb_surface_gralloc.c
@@ -388,9 +388,13 @@ VAStatus psb_CreateSurfacesFromGralloc(
                 obj_surface->share_info->native_window = (void *)external_buffers->reserved[0] ;
 
                 obj_surface->share_info->surface_protected = driver_data->protected;
-
-                obj_surface->share_info->crop_width = driver_data->render_rect.width;
-                obj_surface->share_info->crop_height = driver_data->render_rect.height;
+                if (driver_data->render_rect.width == 0 || driver_data->render_rect.height == 0) {
+                    obj_surface->share_info->crop_width = obj_surface->share_info->width;
+                    obj_surface->share_info->crop_height = obj_surface->share_info->height;
+                } else {
+                    obj_surface->share_info->crop_width = driver_data->render_rect.width;
+                    obj_surface->share_info->crop_height = driver_data->render_rect.height;
+                }
 
                 drv_debug_msg(VIDEO_DEBUG_GENERAL, "%s : Create graphic buffer success"
                                          "surface_id= 0x%x, vaddr[0] (0x%x), vaddr[1] (0x%x)\n",
